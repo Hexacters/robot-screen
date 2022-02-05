92763348d272b5f94fc392a479160100fb3f1074
Venkata Narayanan V <vnarayananv@ghx.com>
2022-02-05 12:21:25 +0530

    UI Update

---
+++ attendance.js
@@ -0,0 +1,54 @@
+var robot = require("robotjs");
+var momnet = require('moment');
+var cp = require('child_process');
+
+// Speed up the mouse.
+robot.setMouseDelay(2);
+
+const duleFn = (start, end, t = 1) => {
+    robot.keyToggle(start, 'down');
+    for (var i = 0; i < t; i++) {
+        robot.keyTap(end);
+    }
+    robot.keyToggle(start, 'up')
+}
+
+const toggle = () => {
+    robot.moveMouseSmooth(393, 162);
+    duleFn('command', '3');
+    duleFn('control', 't');
+
+    robot.typeString('https://people.mookambikainfo.com/');
+
+    // Press enter.
+    robot.keyTap("enter");
+
+    setTimeout(() => {
+        robot.mouseClick();
+    }, 10000);
+}
+
+
+
+const start = async () => {
+    const isCheckIn = momnet().isBetween(momnet().hour(9).minute(28), momnet().hour(9).minute(35));
+    const isCheckout = momnet().isBetween(momnet().hour(21).minute(28), momnet().hour(21).minute(33));
+    console.clear();
+    console.log("Zoho Attendance - Auto Check-in check-out")
+    console.log("Last Check at: - " + momnet().format('hh:mm:ss A'));
+
+    if (isCheckIn) {
+        toggle();
+        cp.fork(__dirname + '/index.js');
+        return;
+    }
+
+    if (isCheckout) {
+        toggle();
+        return;
+    }
+}
+
+// Start the Program
+setInterval(start, 5000 * 60)
+start();
